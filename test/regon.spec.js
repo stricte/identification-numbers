06b7f4c2a66bf31320612d5c128566f30cec2d97
danielpacak <pacak.daniel@gmail.com>
2016-09-19 12:29:45 +0200

    feat: add support for 14 digit REGON numbers
    Closes #2

--- test/regon.spec.js
+++ test/regon.spec.js
@@ -6,10 +6,21 @@ var expect = chai.expect;
 
 describe('regon', function () {
 
-  describe('#random', function () {
-    it('should return random REGON', function () {
-      var regon1 = regon().random();
-      var regon2 = regon().random();
+  describe('#random9', function () {
+    it('should return random 9 digit REGON', function () {
+      var regon1 = regon().random9();
+      var regon2 = regon().random9();
+
+      expect(regon1).not.to.equal(regon2);
+      expect(regon(regon1).isValid()).to.be.true;
+      expect(regon(regon2).isValid()).to.be.true;
+    });
+  });
+
+  describe('#random14', function () {
+    it('should return random 14 digit REGON', function () {
+      var regon1 = regon().random14();
+      var regon2 = regon().random14();
 
       expect(regon1).not.to.equal(regon2);
       expect(regon(regon1).isValid()).to.be.true;
@@ -18,12 +29,20 @@ describe('regon', function () {
   });
 
   describe('#isValid', function () {
-    it('should return true for valid REGON', function () {
+    it('should return true for valid 9 digit REGON', function () {
       expect(regon('550440322').isValid()).to.be.true;
     });
 
-    it('should return true for valid REGON with checksum equal 10', function () {
+    it('should return true for valid 9 digit REGON with checksum equal 10', function () {
       expect(regon('386685250').isValid()).to.be.true;
+    });
+
+    it('should return true for valid 14 digit REGON', function () {
+      expect(regon('12345678512347').isValid()).to.be.true;
+    });
+
+    it('should return false for 14 digit REGON with invalid checksum', function () {
+      expect(regon('12345678512346').isValid()).to.be.false;
     });
 
     it('should return false for too long REGON', function () {
